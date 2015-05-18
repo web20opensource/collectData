c46a22fc41bcbd11a53d1b1119863b9de227fdb2
Mario Ruiz <web2.0opensource@gmail.com>
2015-05-18 15:58:34 -0500

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-
-app = express();
-app.use(express.static(__dirname));
-
-app.listen(8081,'69.28.91.49', function(){
-  console.log("Run your tests at: http://localhost:8081");
-});
